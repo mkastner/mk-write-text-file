2cb6d2c9ed868975f033f2736f3ebfd653a4da6f
Michael Kastner <kastner@galt.de>
2017-01-04 08:14:17 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,3 @@
+/* jslint node: true, strict: implied, esversion: 6 */
+var writeTextFilePromise = require('lib/write-text-file');
+module.exports = writeTextFilePromise;
---
+++ test/mk-write-text-file-test.js
@@ -0,0 +1,13 @@
+/* jslint node: true, strict:implied, esversion: 6 */
+
+var tape = require('tape'),
+    fs = require('fs'),
+    deleteFile = ('mk-delete-file');
+
+tape('mk write text file', function(t) {
+
+    t.plan(1);
+
+
+
+});
